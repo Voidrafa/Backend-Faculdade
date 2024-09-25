88093a1d4a98586db19d8f330148f21a5e26f7fd
Voidrafa <rafaelmanoelss03@gmail.com>
2024-09-25 17:45:48 -0300

    Change learn to ass kill

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn git
+          Learn ass kill
         </a>
       </header>
     </div>
