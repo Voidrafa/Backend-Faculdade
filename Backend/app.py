4c9d37e3c444d5401ca05ef0e942951233ea4f72
Voidrafa <rafaelmanoelss03@gmail.com>
2024-11-02 02:30:27 -0300

    FIX WORKS ON SHIT PC

--- Backend/app.py
+++ Backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
+from flask_cors import CORS
+
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 # Conexão com MongoDB via variável de ambiente
 client = MongoClient(os.getenv("MONGO_URI"))
